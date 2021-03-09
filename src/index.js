918cbe94ca7065167ea81f9e64c9c70d84e97b72
mairelis-montilla <mairelismontilla@gmail.com>
2021-03-08 19:08:11 -0500

    feat ask if path absolute. close #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const mdLinks = () => {
+const path = require('path');
 
+const mdLinks = (pathUser) => {
+  const isAbsolute = path.isAbsolute(pathUser);
+
+  return console.log('soy el resultado', isAbsolute);
 };
 
+mdLinks('/home/andres/Laboratoria/Practica');
+mdLinks('12.14');
 module.exports = mdLinks;
